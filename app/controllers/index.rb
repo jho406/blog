a0ec12c31ab0f6ba97751bc0b827427d7ffffba3
Michelle Zhang <mzhang216@Michelles-MacBook-Air.local>
2013-04-04 15:06:20 -0700

    added initial controllers and views

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,16 @@
 get '/' do
   # Look in app/views/index.erb
+  @posts = Post.all
   erb :index
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/new' do
+  post = Post.new(params[:post])
+  tags = params[:tags].split(",")
+  tags.each{|name| post.tags.create(:name=>name)}
+  redirect to("/")
 end
