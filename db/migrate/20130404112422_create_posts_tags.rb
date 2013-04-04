25d91132a77756d0bb8b069ca3a45c3e0348980d
Michelle Zhang <mzhang216@Michelles-MacBook-Air.local>
2013-04-04 15:05:41 -0700

    fixed issues with timestamps in intermediary

--- db/migrate/20130404112422_create_posts_tags.rb
+++ db/migrate/20130404112422_create_posts_tags.rb
@@ -2,9 +2,7 @@ class CreatePostsTags < ActiveRecord::Migration
   def change
     create_table :posts_tags do |t|
       t.references :post
-      t.references :tag
-      
-      t.timestamps
+      t.references :tag      
     end
   end
 end
